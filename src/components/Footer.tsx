e49503303a6e1e62d308c2592708c2a874fe9fff
yellhtetaung <yehtetaung.dev@gmail.com>
2023-11-28 23:59:15 +0630

    deploying: edited footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div className='bg-[#A8BA57] w-full h-auto'>
+        <div className='bg-[#A8BA57] w-full h-auto p-20 text-3xl text-white font-bold'>
             <h1>Myanmar Constitutional Tribunal Office</h1>
         </div>
     );
