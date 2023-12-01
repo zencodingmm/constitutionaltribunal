2a065a46e0d9a962a10ede787e1aeae5e601915c
papahmwe <doublehmwenetmsi123@gmail.com>
2023-12-01 13:54:22 +0630

    papahmwe: add news component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import './globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import Home from './page';
+import News from '@/components/Home/News';
 
 export const metadata: Metadata = {
     title: 'Create Next App',
@@ -13,8 +15,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang='en'>
             <body>
                 <Header />
+                <News />
                 {children}
-                <Footer />
             </body>
         </html>
     );
