eee305ea37f53640c3a7cacc7d4b7f202b4ea03c
ArkarPhyo8 <arkarphyo200156@gmail.com>
2023-12-09 14:58:37 +0630

    arkarphyo:edit css

--- src/components/navbar/Dropdown.tsx
+++ src/components/navbar/Dropdown.tsx
@@ -9,15 +9,15 @@ const Dropdown = ({ submenus, dropdown, depthLevel }: Any) => {
     depthLevel = depthLevel + 1;
 
     const dropdownClass = depthLevel > 1 ? 'left-full top-0 min-[769px]:absolute' : '';
-    const dropdownLength2 = submenus.length === 2 ? 'h-[100px]' : '';
-    const dropdownLength3 = submenus.length === 3 ? 'h-[140px]' : '';
-    const dropdownLength4 = submenus.length === 4 ? 'h-[180px]' : '';
-    const dropdownLength5 = submenus.length === 5 ? 'h-[220px]' : '';
+    const dropdownLength2 = submenus.length === 2 ? 'min-[769px]:h-[100px]' : '';
+    const dropdownLength3 = submenus.length === 3 ? 'min-[769px]:h-[140px]' : '';
+    const dropdownLength4 = submenus.length === 4 ? 'min-[769px]:h-[180px]' : '';
+    const dropdownLength5 = submenus.length === 5 ? 'min-[769px]:h-[220px]' : '';
 
     return (
         <ul
-            className={`transition-all duration-300 z-50 min-[769px]:bg-[--navbar-color] flex flex-col gap-1 min-w-fit right-0 left-0 text-xl pl-5  pr-0  min-[769px]:rounded min-[769px]:absolute md:px-5 ${dropdownClass} ${
-                dropdown ? `${dropdownLength2} ${dropdownLength3} ${dropdownLength4} ${dropdownLength5} py-5 opacity-100` : `opacity-0 h-0 overflow-hidden`
+            className={`flex flex-col gap-1 min-w-fit text-xl right-0 left-0 pl-5 pr-0 transition-all duration-300 z-50 min-[769px]:bg-[--navbar-color] min-[769px]:rounded min-[769px]:absolute min-[769px]:px-5 ${dropdownClass} ${
+                dropdown ? `${dropdownLength2} ${dropdownLength3} ${dropdownLength4} ${dropdownLength5} py-5 min-[768px]:opacity-100` : `opacity-0 h-0 overflow-hidden`
             }`}
         >
             {submenus.map((submenu: any, index: number) => (
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,15 +7,15 @@ const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
     return (
-        <nav className={`bg-[--navbar-color] w-full z-50 py-5 sticky top-0 mt-[1rem] md:mt-0`}>
+        <nav className={`bg-[--navbar-color] w-full z-50 py-2 sticky top-0 mt-[1rem] min-[769px]:py-1`}>
             <div className='w-full max-[768px]:relative'>
                 <div className='w-[90%] mx-auto'>
                     <div className='flex justify-between'>
-                        <span className='min-[769px]:hidden font-medium text-[--bg-first] hover:text-[--text-hover]'></span>
+                        <span className='font-medium text-[--bg-first] hover:text-[--text-hover] min-[769px]:hidden'>menu</span>
                         <div className='min-[769px]:hidden'>{menuOpen ? <X onClick={() => setMenuOpen((prev) => !prev)} /> : <Menu onClick={() => setMenuOpen((prev) => !prev)} />}</div>
                     </div>
                     <ul
-                        className={`bg-[--navbar-color] flex gap-2 px-3 pt-2 pb-5 h-[80vh] w-full transition-all duration-300 ease-in-out max-[768px]:overflow-y-auto min-[769px]:h-auto  min-[769px]:py-2 max-[768px]:absolute max-[768px]:top-[32px] lg:gap-3 min-[769px]:items-center min-[769px]:flex-wrap max-[768px]:flex-col ${
+                        className={`bg-[--navbar-color] w-full h-[80vh] flex gap-2 px-3 pt-2 pb-5 transition-all duration-300 ease-in-out max-[768px]:flex-col max-[768px]:absolute max-[768px]:overflow-y-auto max-[768px]:top-[32px] min-[769px]:h-auto min-[769px]:py-2 min-[769px]:items-center min-[769px]:flex-wrap ${
                             menuOpen ? 'left-0' : 'left-[-100%]'
                         }`}
                     >
